c42bd846cd704baa1ca117199db8dea59070a47e
kalanamj <kalanamihiranga9@hotmail.com>
2025-03-09 22:47:33 +0530

    fixed whitelisting url not identified issue

--- src/main/java/com/netwizsoft/spring_security_latest/controller/security/JwtAuthenticationFilter.java
+++ src/main/java/com/netwizsoft/spring_security_latest/controller/security/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
@@ -19,9 +20,11 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
@@ -32,10 +35,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
+
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWithIgnoreCase(authHeader, "Bearer ")) {
+        if (Objects.isNull(authHeader) || !StringUtils.startsWithIgnoreCase(authHeader, "Bearer ")) {
+            log.info("Skipping due to no Authorization header in request");
             filterChain.doFilter(request, response);
             return;
         }
